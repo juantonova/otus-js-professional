c8afc47bed2314564c2867efdc0b8b185d993e54
antonova.u <antonova.u@ati.su>
2024-01-26 23:50:02 +0300

    file-system: print count of items and folders

--- file-system/file-system.js
+++ file-system/file-system.js
@@ -3,7 +3,7 @@ const print = require('../tree-function/tree-function');
 
 const readDirectories = async (path, level, step = 0) => {
   const name = path.split('/').at(-1);
-  const result = { name, items: [] };
+  const result = { name, items: [], directories: 0, files: 0 };
   try {
     step++;
     if (level < step) return result;
@@ -13,8 +13,11 @@ const readDirectories = async (path, level, step = 0) => {
       if (stat.isDirectory()) {
         const dir = await readDirectories(`${path}/${item}`, level, step);
         result.items.push(dir);
+        result.directories += dir.directories + 1;
+        result.files += dir.files
       } else {
         result.items.push({ name: item });
+        result.files++;
       }
     }
     return result;
@@ -23,8 +26,18 @@ const readDirectories = async (path, level, step = 0) => {
   }
 }
 
+const printCountOfItems = ({ files, directories }) => {
+  console.log(`${files} files, ${directories} directories`);
+}
+
+const printAllInfo = (tree) => {
+  print(tree);
+  printCountOfItems(tree);
+}
+
+
 const path = process.argv[2] || 'file-system/example';
 const level = process.argv[3] || 2;
 
 
-readDirectories(path, level).then(tree => print(tree));
+readDirectories(path, level).then(tree => printAllInfo(tree));
